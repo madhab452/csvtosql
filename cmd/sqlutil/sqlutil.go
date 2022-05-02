3dbdbae42908286d51578a7ea1ea95f24c46158a
Madhab Acharya <madhaba@rabbit.co.th>
2022-05-02 18:54:59 +0700

    Improve table name

--- cmd/sqlutil/sqlutil.go
+++ cmd/sqlutil/sqlutil.go
@@ -12,10 +12,27 @@ func ToColumnName(header string) string {
 	return toSQLFriendlyName(header)
 }
 
-// ToTableName returns a nice name for table
+// ToTableName returns a nice name for table.
+// example given ./csvs/BTC-USD-2.csv returns btc_usd_2
+// :future-improvements using regex.
 func ToTableName(fname string) string {
-	// return toSqlFriendlyName(fname) // todo: create table name from file
-	return "csvtosql"
+	var res []byte
+	for i, _ := range fname {
+		ch := fname[i]
+		if ch == '/' { // reset if its a path, not a filename
+			res = []byte{}
+		} else if ch == '.' {
+			break
+		} else {
+			if ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch >= '0' && ch <= '9' || ch == '_' {
+				res = append(res, ch)
+			} else {
+				res = append(res, '_')
+			}
+		}
+	}
+
+	return strings.ToLower(string(res))
 }
 
 func toSQLFriendlyName(str string) string {
